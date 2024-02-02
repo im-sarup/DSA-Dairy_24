16288563ba6ed8b1899e28e72dd0de65e2f81827
Fajle Sarup Hossain <105408200+im-sarup@users.noreply.github.com>
2024-02-02 13:13:47 +0530

    Add files via upload

---
+++ 3.1.1. Linked List Structures.cpp
@@ -0,0 +1,55 @@
+//Linked List Structures
+// ---------------------
+
+// In C
+#include<stdio.h>
+Struct Node {
+    int data;
+    struct Node* next;
+};
+
+// In C++
+
+// 1. Singly Linked List
+// #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+Class Node {    
+    int data;
+    Node* next;     
+};
+
+
+//2. Doubly Linked List
+#include<bits/stdc++.h>
+using namespace std;
+
+Class Node {
+    int data;
+    Node* next, prev;
+};
+
+// 3. Circular Linked List
+#include<bits/stdc++.h>
+using namespace std;
+
+Class Node {
+    public:
+    int data;
+    Node* next;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
